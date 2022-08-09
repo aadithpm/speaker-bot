44f6b161d201b566f86f998d916b7c3c21215625
Aadith PM <aadithpm@gmail.com>
2022-08-08 21:58:36 -0500

    Welcome message progress for GuildMemberAdd handler

--- cmd/main.go
+++ cmd/main.go
@@ -12,36 +12,32 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type Config struct {
-	Token string `json:"token"`
-}
-
 var Session *discordgo.Session
 var Token string
 
-func init() {
+func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Warn("No .env file found")
+		log.Warn("error loading .env")
 	}
 
 	Token = os.Getenv("DISCORD_BOT_TOKEN")
-}
-
-func init() {
-	Session, _ = discordgo.New(Token)
-	Session.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAll)
-}
-
-func main() {
-	err := Session.Open()
-	if err != nil {
-		log.Fatal("error opening connection to Discord: %s\n", err)
+	if Token == "" {
+		log.Fatal("no bot token found, exiting..")
 		os.Exit(1)
 	}
 
+	Session, _ = discordgo.New(Token)
+	Session.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAll)
+
 	handlers.AddHandlers(Session)
 
-	log.Info("Bot is running")
+	err := Session.Open()
+	if err != nil {
+		log.Fatal("error opening connection to Discord: %v\n", err)
+		os.Exit(1)
+	}
+
+	log.Info("bot is running")
 	log.Info(Session.Identify.Intents)
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 	log "github.com/sirupsen/logrus"
 )
@@ -8,17 +10,50 @@ import (
 func AddHandlers(s *discordgo.Session) {
 	s.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		log.Info(m.Message.Content)
+		// TODO do something with this later
 	})
 
 	s.AddHandler(func(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
-		log.Info("Member joined")
-		log.Infof("%s with ID %s joined the guild %s at %s", m.Member.User.Username, m.Member.User.ID, m.GuildID, m.JoinedAt)
+		log.Info("member joined")
+		log.Infof("%v with ID %v joined the guild %v at %v", m.Member.User.Username, m.Member.User.ID, m.GuildID, m.JoinedAt)
 
-		channels, err := s.GuildChannels(m.GuildID)
+		c, err := s.GuildChannels(m.GuildID)
 		if err != nil {
-			log.Warnf("Error getting guild channels for %s: %s", m.GuildID, err)
+			log.Warnf("error getting guild channels for %v: %v", m.GuildID, err)
 		}
-		log.Infof("%s", channels)
+		// Send a welcome message
+		// TODO let user configure the channel and the message
+		tc, err := GetChannelByName(c, "bot-tests")
+		if err != nil {
+			log.Warnf("error getting channel name %v")
+		}
 
+		msg := fmt.Sprintf("Hello <@%s>", m.User.ID)
+		if !m.User.Bot {
+			SendMessageInChannel(s, tc, msg)
+		}
 	})
+}
+
+// Gets channel by name from list of channels, sets err if not found
+func GetChannelByName(c []*discordgo.Channel, n string) (channel *discordgo.Channel, err error) {
+	for _, channel := range c {
+		log.Infof("channel %v %v %v", channel.Name, channel.ID, channel.Type)
+		if channel.Name == n {
+			return channel, nil
+		}
+	}
+	return nil, fmt.Errorf("channel name %v not found", n)
+}
+
+// Sends message in channel, sets err if failed
+func SendMessageInChannel(s *discordgo.Session, c *discordgo.Channel, m string) {
+	id := c.ID
+
+	msg, err := s.ChannelMessageSend(id, m)
+	if err != nil {
+		log.Warn("error sending msg {%v} in channel %v %v: %v", msg.Content, c.Name, id, err)
+		return
+	}
+	log.Infof("sent msg %v in channel %v %v", msg.Content, c.Name, id)
 }
