b34480b6b0fd7952657809d3252e9a01c99ecfa4
Aadith PM <aadithpm@gmail.com>
2024-02-28 11:40:31 -0600

    :bug: Handle quotes in LLaMA prompt

--- internal/commands/ai_command.go
+++ internal/commands/ai_command.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/aadithpm/speaker-bot/internal/utils"
 	"github.com/bwmarrin/discordgo"
@@ -58,9 +59,10 @@ func (a AiCommand) GetName() string {
 func (a AiCommand) Handler(s *discordgo.Session, d *discordgo.ApplicationCommandInteractionData) (res string, emb *discordgo.MessageEmbed, err error) {
 	m := utils.GetOptionsMap(d.Options)
 	p := m["prompt"].StringValue()
+	payload := fmt.Sprintf(`{"prompt": %s}`, strconv.Quote(p))
 
-	jsonStr := []byte(fmt.Sprintf(`{"prompt": "%s"}`, p))
-	log.Infof("making POST request to worker..")
+	jsonStr := []byte(payload)
+	log.Infof("making POST request to worker with payload %s..", payload)
 	resp, err := http.Post("https://malahayati-discord-ai.aadith-pm.workers.dev", "application/json", bytes.NewBuffer(jsonStr))
 	if err != nil || resp.StatusCode != http.StatusOK {
 		log.Errorf("error making request to workers: %v", err)
@@ -74,15 +76,16 @@ func (a AiCommand) Handler(s *discordgo.Session, d *discordgo.ApplicationCommand
 	log.Info(bodyString)
 	if err != nil {
 		log.Errorf("error reading body: %v", err)
-		return "", nil, err
+		return "", nil, fmt.Errorf("TEXT GENERATION FAILED - CRITICAL ERROR")
 	}
 	err = json.Unmarshal(body, &workersResponse)
 	if err != nil {
 		log.Errorf("error unmarshaling: %v", err)
-		return "", nil, err
+		return "", nil, fmt.Errorf("TEXT GENERATION FAILED - CRITICAL ERROR")
 	}
 	if len(workersResponse[0].Response.Response) > 0 {
 		return workersResponse[0].Response.Response, nil, nil
 	}
-	return "", nil, fmt.Errorf("workers response was empty")
+	log.Errorf("workers response empty")
+	return "", nil, fmt.Errorf("TEXT GENERATION FAILED - CRITICAL ERROR")
 }
